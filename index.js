d7008bd54ebec06d73fc8e31fe546149cbed0f8b
riya nagre <ranisnagre@gmail.com>
2018-08-05 12:48:09 +0400

    new file created in models

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const users = require('./models/users');
 const products = require('./models/products');
 const results = require('./models/results');
+const employees = require('./models/employees');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser());
 app.use('/api/users', users);
 app.use('/api/products', products);
 app.use('/api/results', results);
+app.use('/api/employees', employees);
 
 app.get('/', function (req, res) {
     res.send('This is my landing page without any endpoint');
---
+++ models/employees.js
@@ -0,0 +1,104 @@
+let express = require('express');
+let connection = require('../config/mysql-connection');
+let router = express.Router();
+
+router.get('/', function (req, res) {
+    let query = `SELECT * from employees`;
+
+    connection.query(query, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.length == 0) {
+            res.send({ 'result': 'id is not found' });
+        } else {
+            res.send(results);
+        }
+
+    });
+});
+
+router.get('/:id', function (req, res) {
+    let id = req.params.id;
+    let query = `SELECT * from employees where id = ${id}`;
+
+    connection.query(query, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.length == 0) {
+            res.send({ 'results': 'id is not found' });
+        } else {
+            res.send(results);
+        }
+    });
+});
+
+router.put('/:id', function (req, res) {
+    let id = req.params.id;
+    let name = req.body.name;
+    let addr = req.body.addr;
+    let salary = req.body.salary;
+    let age = req.body.age;
+    let query = `UPDATE employees SET
+                    name = "${name}",
+                    addr = "${addr}",
+                    salary = "${salary}",
+                    age = "${age}"
+            WHERE id = ${id}
+            `;
+    console.log(query);
+
+    connection.query(query, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.affectedRows == 0) {
+            res.send({ 'results': 'Updated unsuccessfully' });
+        } else {
+            res.send({ 'results': 'Updated successfully' });
+        }
+    });
+});
+
+router.post('/', function (req, res) {
+
+    let name = req.body.name;
+    let addr = req.body.addr;
+    let salary = req.body.salary;
+    let age = req.body.age;
+    let query = `INSERT INTO employees SET
+                    name = "${name}",
+                    addr = "${addr}",
+                    salary = "${salary}",
+                    age = "${age}"
+            `;
+    console.log(query);
+
+    connection.query(query, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.affectedRows == 0) {
+            res.send({ 'results': 'Inserted unsuccessfully' });
+        } else {
+            res.send({ 'results': 'Inserted successfully' });
+        }
+    });
+});
+
+router.delete('/:id', function (req, res) {
+    let id = req.params.id;
+    connection.query(`delete from employees where id = ${id}`, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.affectedRows == 0) {
+            res.send({ 'result': 'Deleted unsuccessfully' });
+        } else {
+            res.send({ 'result': 'Deleted successfully' });
+        }
+    });
+});
+
+module.exports = router;
