1c78312cbee48f1072f65f8b01b079bf57108f39
riya nagre <ranisnagre@gmail.com>
2018-08-07 13:25:30 +0400

    add one new function . id function

--- models/finalResults.js
+++ models/finalResults.js
@@ -16,4 +16,21 @@ router.get('/', function (req, res) {
 
     });
 });
+router.get('/:id', function (req, res) {
+    let id = req.params.id;
+    let query = `SELECT * from finalResults WHERE id ${id}`;
+
+    connection.query(query, function (error, results) {
+        if (error) {
+            throw error;
+        }
+        if (results.length == 0) {
+            res.send({ 'result': 'id is not found' });
+        } else {
+            res.send(results);
+        }
+
+    });
+});
+
 module.exports = router;
